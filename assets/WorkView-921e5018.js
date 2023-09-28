d4b5f1be6898281a2547d16ed9d1a6c8bfaae740
vjollcc <146346564+vjollcc@users.noreply.github.com>
2023-09-28 17:31:42 +0200

    Add files via upload

---
+++ assets/WorkView-921e5018.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,a as t}from"./index-f30d3f1d.js";const _={},c={class:"about"},a=t("h1",null,"This is an about page",-1),n=[a];function r(i,d){return o(),s("div",c,n)}const u=e(_,[["render",r]]);export{u as default};
